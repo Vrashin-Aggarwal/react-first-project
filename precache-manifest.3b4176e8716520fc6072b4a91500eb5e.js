afb3293221774b149fd97c940dd6691dc01d5252
vrashin.aggarwal <vrashin.aggarwal@paytm.com>
2020-04-18 01:49:56 +0530

    Updates

---
+++ precache-manifest.3b4176e8716520fc6072b4a91500eb5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e554a1f61559ff7892d397292e43884f",
+    "url": "/react-first-project/index.html"
+  },
+  {
+    "revision": "09dd5587fd7eef883ec3",
+    "url": "/react-first-project/static/css/main.bd4e1b0d.chunk.css"
+  },
+  {
+    "revision": "273410781497198085b2",
+    "url": "/react-first-project/static/js/2.85c3c5d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-first-project/static/js/2.85c3c5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09dd5587fd7eef883ec3",
+    "url": "/react-first-project/static/js/main.8e1a05c1.chunk.js"
+  },
+  {
+    "revision": "ca57b7f6cf4f57d95a39",
+    "url": "/react-first-project/static/js/runtime-main.ddbc17e3.js"
+  }
+]);
